0a36aced8bf6903165c856dadfcdc212582c0f58
Margarit Hakobyan <margarit.hakobyan@improving.com>
2022-12-21 14:36:11 -0800

    Fix indentation
    Signed-off-by: Margarit Hakobyan <margarit.hakobyan@improving.com>

--- src/test/java/org/opensearch/jdbc/types/TimeTypeTest.java
+++ src/test/java/org/opensearch/jdbc/types/TimeTypeTest.java
@@ -40,12 +40,12 @@ public class TimeTypeTest {
 
   @ParameterizedTest
   @CsvSource(value = {
-          "00:00:00, 00:00:00",
-          "01:01:01, 01:01:01",
-          "23:59:59, 23:59:59",
-          "1880-12-22 00:00:00, 00:00:00",
-          "2000-01-10 01:01:01, 01:01:01",
-          "1998-08-17 23:59:59, 23:59:59"
+      "00:00:00, 00:00:00",
+      "01:01:01, 01:01:01",
+      "23:59:59, 23:59:59",
+      "1880-12-22 00:00:00, 00:00:00",
+      "2000-01-10 01:01:01, 01:01:01",
+      "1998-08-17 23:59:59, 23:59:59"
   })
   void testTimeFromStringWithCalendar(String inputString, String resultString) {
     Time time = Assertions.assertDoesNotThrow(
